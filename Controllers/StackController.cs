e1e56072f5e22d242d6b9fa84318633d6e1c2de0
M Fariz Wisnu prananda <muhamadfarizwisnuprananda@gmail.com>
2024-08-28 22:56:27 +0700

    fix: error to database, solution add user fariz.wisnu into database

---
+++ Controllers/StackController.cs
@@ -0,0 +1,33 @@
+﻿using learn_c__api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learn_c__api.Controllers;
+[Route("api/stock")]
+[ApiController]
+public class StockController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    public StockController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var stocks = _context.Stocks.ToList();
+        return Ok(stocks);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById([FromRoute] int id)
+    {
+        var stock = _context.Stocks.Find(id);
+        if (stock == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(stock);
+    }
+}
--- obj/Debug/net8.0/learn-c#-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/learn-c#-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn-c#-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e297057de61ae15576f20d82c482442554983a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn-c#-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn-c#-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
