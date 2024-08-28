8e297057de61ae15576f20d82c482442554983a1
M Fariz Wisnu prananda <muhamadfarizwisnuprananda@gmail.com>
2024-08-28 21:58:50 +0700

    init about learn api with .NET

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using learn_c__api.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace learn_c__api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions dbContextOptions) : base  (dbContextOptions) 
+    {
+        
+    }
+    
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
