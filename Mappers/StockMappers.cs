1dd1b9bf4a8acfdbea68bc4ece96709ebcd60a80
M Fariz Wisnu prananda <muhamadfarizwisnuprananda@gmail.com>
2024-08-29 21:45:52 +0700

    implement dtos

---
+++ Mappers/StockMappers.cs
@@ -0,0 +1,22 @@
+﻿using learn_c__api.Dtos.Stock;
+using learn_c__api.models;
+
+namespace learn_c__api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+               Id = stockModel.Id,
+               Symbol = stockModel.Symbol,
+               CompanyName = stockModel.CompanyName,
+               Purchase = stockModel.Purchase,
+               LastDiv = stockModel.LastDiv,
+               Industry = stockModel.Industry,
+               MarketCap = stockModel.MarketCap
+            };
+        }
+    }
+}
